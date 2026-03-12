5f46222f8a1c53989005de8a47998252d1077771
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com.>
2026-03-12 15:42:41 +0200

    feat: integrate mongodb and mongoose for database operations

--- app.ts
+++ app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import genres from './routes/genres.js';
-
+import mongoose from 'mongoose';
 const app = express();
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.error('Could not connect to MongoDB...', err));
+
 app.use(express.json());
-app.use('api/genres', genres);
+app.use('/api/genres', genres);
 
 export default app;
--- routes/genres.ts
+++ routes/genres.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import type { Request, Response } from 'express';
 import Joi from 'joi';
+import mongoose, { Schema, model, Document } from 'mongoose';
 
-interface Genre {
-  id: number;
+interface IGenre extends Document {
   name: string;
 }
 
@@ -11,67 +11,82 @@ interface GenreInput {
   name: string;
 }
 
+const genreSchema = new Schema<IGenre>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+
+const Genre = model<IGenre>('Genre', genreSchema);
+
 const router = Router();
 
-const genres: Genre[] = [
-  { id: 1, name: 'Action' },
-  { id: 2, name: 'Horror' },
-  { id: 3, name: 'Romance' },
-];
-
-router.get('/', (_: Request, res: Response) => {
+router.get('/', async (_: Request, res: Response) => {
+  const genres = await Genre.find().sort('name');
   res.send(genres);
 });
 
-router.get('/:id', (req: Request, res: Response) => {
-  const genre = genres.find((g) => g.id === parseInt(String(req.params.id!)));
-  if (!genre)
-    return res.status(404).send('The genre with the given ID was not found.');
-  res.send(genre);
-});
-
-router.post('/', (req: Request<{}, {}, GenreInput>, res: Response) => {
+router.post('/', async (req: Request<{}, {}, GenreInput>, res: Response) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0]?.message);
 
-  const genre: Genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
 
-  genres.push(genre);
   res.status(201).send(genre);
 });
 
 router.put(
   '/:id',
-  (req: Request<{ id: string }, {}, GenreInput>, res: Response) => {
-    const genre = genres.find((g) => g.id === parseInt(String(req.params.id!)));
-    if (!genre)
-      return res.status(404).send('The genre with the given ID was not found.');
+  async (req: Request<{ id: string }, {}, GenreInput>, res: Response) => {
+    if (!mongoose.Types.ObjectId.isValid(String(req.params.id!)))
+      return res.status(400).send('Invalid ID.');
 
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0]?.message);
 
-    genre.name = req.body.name;
+    const genre = await Genre.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { returnDocument: 'after' },
+    );
+
+    if (!genre)
+      return res.status(404).send('The genre with the given ID was not found.');
+
     res.send(genre);
   },
 );
 
-router.delete('/:id', (req: Request, res: Response) => {
-  const genre = genres.find((g) => g.id === parseInt(String(req.params.id!)));
+router.delete('/:id', async (req: Request, res: Response) => {
+  if (!mongoose.Types.ObjectId.isValid(String(req.params.id!)))
+    return res.status(400).send('Invalid ID.');
+
+  const genre = await Genre.findByIdAndDelete(req.params.id);
+
   if (!genre)
     return res.status(404).send('The genre with the given ID was not found.');
 
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
+  res.send(genre);
+});
+
+router.get('/:id', async (req: Request, res: Response) => {
+  if (!mongoose.Types.ObjectId.isValid(String(req.params.id!)))
+    return res.status(400).send('Invalid ID.');
+
+  const genre = await Genre.findById(req.params.id);
+  if (!genre)
+    return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
 });
 
 function validateGenre(genre: GenreInput) {
   const schema = Joi.object<GenreInput>({
-    name: Joi.string().min(3).required(),
+    name: Joi.string().min(5).max(50).required(),
   });
 
   return schema.validate(genre);
